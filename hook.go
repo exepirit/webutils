96ff515e30e549da27b11e31f7b27ebbbf0e4acc
exepirit <exepir1t@protonmail.com>
2021-04-03 14:37:34 +0700

    feat: hooks

---
+++ hook.go
@@ -0,0 +1,74 @@
+package hook
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/maxence-charriere/go-app/v8/pkg/app"
+)
+
+type Hook interface {
+	SetResult(interface{}) Hook
+	SetError(*error) Hook
+	Use(composer app.Composer, ctx app.Context)
+}
+
+type Action func(app.Context) (interface{}, error)
+
+type baseHook struct {
+	result interface{}
+	error  interface{}
+	act    Action
+}
+
+func NewHook(act Action) Hook {
+	return &baseHook{
+		result: nil,
+		error:  nil,
+		act:    act,
+	}
+}
+
+func (hook *baseHook) SetResult(v interface{}) Hook {
+	hook.result = hook.getPointer(v)
+	return hook
+}
+
+func (hook *baseHook) SetError(err *error) Hook {
+	hook.error = hook.getPointer(err)
+	return hook
+}
+
+func (hook *baseHook) Use(composer app.Composer, ctx app.Context) {
+	ctx.Async(func() {
+		result, err := hook.act(ctx)
+		ctx.Dispatch(func() {
+			hook.indirectSet(hook.result, result)
+			hook.indirectSet(hook.error, err)
+			if err != nil {
+				fmt.Println(err)
+			}
+			composer.Update()
+		})
+	})
+}
+
+func (hook *baseHook) indirectSet(field interface{}, value interface{}) {
+	if field == nil || value == nil {
+		return
+	}
+	v := reflect.ValueOf(value)
+	target := reflect.Indirect(reflect.ValueOf(field))
+	if !v.Type().AssignableTo(target.Type()) {
+		panic(fmt.Errorf("target type (%s) is not assignable to %s", target.Type(), v.Type()))
+	}
+	target.Set(v)
+}
+
+func (baseHook) getPointer(v interface{}) interface{} {
+	vv := reflect.ValueOf(v)
+	if vv.Kind() == reflect.Ptr {
+		return v
+	}
+	return reflect.New(vv.Type()).Interface()
+}
